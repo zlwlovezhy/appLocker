be7ae4d206ae016f7aa8c97fdee8fb7428dbacae
zlwlovezhy <3207632593@qq.com>
2026-07-07 09:08:06 +0800

    Add swipe refresh layout dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,7 +52,7 @@ dependencies {
 
     implementation("androidx.room:room-runtime:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
-    ksp("androidx.room:room-compiler:2.6.1")
+      ksp("androidx.room:room-compiler:2.6.1")
 
+    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
-}
